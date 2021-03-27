d588751c34b995ddc3641d8e618fc791353f8940
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-03-27 17:37:20 -0300

    feat: listar todos os produtos

---
+++ product-api/src/main/java/com/alps/shop/productapi/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.alps.shop.productapi.service;
+
+import com.alps.shop.productapi.dto.ProductDTO;
+import com.alps.shop.productapi.model.Product;
+import com.alps.shop.productapi.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductDTO> getAll() {
+        List<Product> products = productRepository.findAll();
+        return products
+                .stream()
+                .map(ProductDTO::convert)
+                .collect(Collectors.toList());
+    }
+
+}
